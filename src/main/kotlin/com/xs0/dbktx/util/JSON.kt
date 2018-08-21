02c93e9e04e76661f54f3dd4d5b7b5ffad41e71c
Mitja Šlenc <mslenc@gmail.com>
2018-08-21 20:53:09 +0200

    tweaked JSON output

--- src/main/kotlin/com/xs0/dbktx/util/JSON.kt
+++ src/main/kotlin/com/xs0/dbktx/util/JSON.kt
@@ -1,7 +1,9 @@
 package com.xs0.dbktx.util
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
@@ -9,17 +11,18 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import kotlin.reflect.KClass
 
 object JSON {
-    val jsonMapper: ObjectMapper
+    private val jsonMapper = ObjectMapper()
 
     init {
-        jsonMapper = ObjectMapper()
-
         jsonMapper.registerModule(KotlinModule())
         jsonMapper.registerModule(ParameterNamesModule())
         jsonMapper.registerModule(Jdk8Module())
         jsonMapper.registerModule(JavaTimeModule())
 
         jsonMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        jsonMapper.configure(SerializationFeature.INDENT_OUTPUT, true)
+
+        jsonMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY)
     }
 
     fun stringify(value: Any): String {
